44c83ff481c2ce97c2b0204170da697615800fb0
s111410540-hub <s111410540@student.nqu.edu.tw>
2026-05-04 10:20:05 +0800

    Delete midterm_Project directory
    重新寫一份專案

--- midterm_Project/drizzle.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'drizzle-kit';
-
-export default defineConfig({
-  schema: './database/schema.ts',
-  out: './drizzle',
-  dialect: 'sqlite',
-});
--- midterm_Project/drizzle/0001_cynical_gunslinger.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `players` ADD `magic_skill` integer DEFAULT 0 NOT NULL;--> statement-breakpoint
-ALTER TABLE `players` ADD `sword_skill` integer DEFAULT 0 NOT NULL;
